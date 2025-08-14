32afcaa17f14a4d6277eada9141615d96a6f89e0
Charlie <charles.ibbett@gmail.com>
2025-08-14 14:55:33 +0100

    feat(new market maker) see prior commit

--- abides-multi-exchange/abides_multi_exchange/agents/__init__.py
+++ abides-multi-exchange/abides_multi_exchange/agents/__init__.py
@@ -3,6 +3,6 @@ from .arbitrage_agent import ArbitrageAgent
 from .momentum_agent import MomentumAgent
 from .noise_agent import NoiseAgent
 from .value_agent import ValueAgent
-from .adapt_mm_single_ex import AdaptiveMarketMakerAgent
+from abides_multi_exchange.agents.market_makers.adapt_mm_single_ex import AdaptiveMarketMakerAgent
 from .core_background_agent import CoreBackgroundAgent
 from .exchange_agent import ExchangeAgent
