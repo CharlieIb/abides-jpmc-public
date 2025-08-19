f143050d7cca4211175a82b2d4326e49416c0f8d
Charlie <charles.ibbett@gmail.com>
2025-08-19 18:36:59 +0100

    MAJOR UPDATE(Value and Momentum agent) Added new version taht uses old strategy on one exchange in the multi exchange environment

--- abides-multi-exchange/abides_multi_exchange/agents/__init__.py
+++ abides-multi-exchange/abides_multi_exchange/agents/__init__.py
@@ -6,3 +6,5 @@ from .value_agent import ValueAgent
 from abides_multi_exchange.agents.market_makers.adapt_mm_single_ex import AdaptiveMarketMakerAgent
 from .core_background_agent import CoreBackgroundAgent
 from .exchange_agent import ExchangeAgent
+from .momentum_agent_se import MomentumAgentSE
+from .value_agent_se import ValueAgentSE
